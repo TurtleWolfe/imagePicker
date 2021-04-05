a8482b5bd0bec777d3b268251e3518df6f173b94
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2021-04-05 18:01:05 -0400

    patched button group

--- components/elements/ImageButtons.tsx
+++ components/elements/ImageButtons.tsx
@@ -10,22 +10,23 @@ export class ImageButtons extends PureComponent {
     }
     this.updateIndex = this.updateIndex.bind(this)
   }
-
   updateIndex(selectedIndex) {
     this.setState({ selectedIndex })
   }
 
-  render() {
-    const buttons = ['Camera', 'Gallery', 'Share']
-    const { selectedIndex } = this.state
+  component1 = () => <Text>Camera</Text>
+  component2 = () => <Text>Gallery</Text>
+  component3 = () => <Text>Share</Text>
 
+  render() {
+    const buttons = [{ element: this.component1 }, { element: this.component2 }, { element: this.component3 }]
+    const { selectedIndex } = this.state
     return (
       <ButtonGroup
         onPress={this.updateIndex}
         selectedIndex={selectedIndex}
         buttons={buttons}
-        containerStyle={{ height: 45, width: 200 }}
-      />
+        containerStyle={{ height: 45, width: 200, overflow: 'visible' }} />
     )
   }
 }
