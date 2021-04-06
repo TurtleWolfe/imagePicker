960ee2bcae7574e65af6b8f74674f9d7aad48be0
TurtleWolfe <7327259+TurtleWolfe@users.noreply.github.com>
2021-04-06 03:48:26 -0400

    back up

---
+++ components/imagePicker copy.tsx
@@ -0,0 +1,137 @@
+import React, { Component } from 'react'
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import logo from '../assets/images/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
+import ImageButtons from '../components/elements/ImageButtons'
+const imagePicker = () => {
+  // render() {
+
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    // console.log(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+    }
+  };
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+        {/* <Text style={styles.instructions}>
+          To share a photo from your phone with a friend,
+          </Text>
+          <Text style={styles.instructions}>
+          just press the button below!
+        </Text> */}
+        <ImageButtons />
+        <TouchableOpacity
+          onPress={openImagePickerAsync}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Take a photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={openImagePickerAsync}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Pick an Image</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Share It</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend,
+          </Text>
+      <Text style={styles.instructions}>
+        just press the button below!
+          </Text>
+      <ImageButtons />
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Take a photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick an Image</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+// }
+
+export default imagePicker
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#888',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#ccc',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 7,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 17,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+    // Try switching it from contain to stretch or cover 
+    marginBottom: 20,
+  }
+});
